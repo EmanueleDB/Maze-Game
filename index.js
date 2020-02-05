4f7d519e123fb1236860e1c2c6271a2fece8ea3b
EmanueleDB <leleoioioi@gmail.com>
2020-02-05 13:14:37 +0100

    vertical walls values updated

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { Engine, Render, Runner, World, Bodies } = Matter;
 
 const width = 600;
 const height = 600;
+const cells = 3;
 
 const engine = Engine.create();
 const { world } = engine;
@@ -27,14 +28,64 @@ const walls = [
 World.add(world, walls);
 
 //Maze generation
-const grid = Array(3)
-  .fill(null)
-  .map(() => Array(3).fill(false));
 
-const verticals = Array(3)
-  .fill(null)
-  .map(() => Array(2).fill(false));
+const shuffle = arr => {
+  let counter = arr.length;
+  while (counter > 0) {
+    const index = Math.floor(Math.random() * counter);
+    counter--;
+    const temp = arr[counter];
+    arr[counter] = arr[index];
+    arr[index] = temp;
+  }
+  return arr;
+};
 
-const horizontals = Array(2)
+const grid = Array(cells)
   .fill(null)
-  .map(() => Array(3).fill(false));
+  .map(() => Array(cells).fill(false));
+
+const verticals = Array(cells)
+  .fill(null)
+  .map(() => Array(cells - 1).fill(false));
+
+const horizontals = Array(cells - 1)
+  .fill(null)
+  .map(() => Array(cells).fill(false));
+
+const startRow = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells);
+
+const stepThroughCell = (row, column) => {
+  if (grid[row][column]) {
+    return;
+  }
+  grid[row][column] = true;
+  const neighbors = shuffle([
+    [row - 1, column, "up"],
+    [row, column + 1, "right"],
+    [row + 1, column, "down"],
+    [row, column - 1, "left"]
+  ]);
+  for (let neighbor of neighbors) {
+    const [nextRow, nextColumn, direction] = neighbor;
+    if (
+      nextRow < 0 ||
+      nextRow >= cells ||
+      nextColumn < 0 ||
+      nextColumn >= cells
+    ) {
+      continue;
+    }
+    if (grid[nextRow][nextColumn]) {
+      continue;
+    }
+    if (direction === "left") {
+      verticals[row][column - 1] = true;
+    } else if (direction === "right") {
+      verticals[row][column] = true;
+    }
+  }
+};
+
+stepThroughCell(startRow, startColumn);
